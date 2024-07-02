60b6f3b7aecf73c20b4b4eda7cf51f94e04095db
Luccas Philot Goncalves <luccasgoncalvrs@gmail.com>
2024-07-02 19:32:02 -0300

    Add funcionalidades em adimn.js

--- admin.js
+++ admin.js
@@ -1,43 +1,63 @@
 const button = document.querySelector('.add')
+const buttonAleatorio = document.querySelector('.aleatorio')
 const input = document.querySelector('.input-codigo')
 const filaCompleta = document.querySelector('.lista')
 
 let minhaListaDeItens = []
 
 function adicionarNovaTarefa() {
+  if (input.value != "") {
     minhaListaDeItens.push({
       tarefa: input.value,
       concluida: false,
     })
-    input.value = ''
-    mostrarTarefas()
+  } else {
+    alert("Número do pedido não pode ser vazio")
+  }
+  input.value = ''
+  mostrarTarefas()
+}
+
+function adicionarAletorio() {
+  const numeroSequencial = minhaListaDeItens.length + 1;
+  minhaListaDeItens.push({
+    tarefa: numeroSequencial,
+    concluida: false,
+  })
+  input.value = ''
+  mostrarTarefas()
 }
 
 function mostrarTarefas() {
-    let novaLi = ''
+  let novaLi = ''
 
-    minhaListaDeItens.forEach((item, posicao) => {
-        novaLi = novaLi +
-          `
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi = novaLi +
+      `
           <li class="filas ${item.concluida && "done"}">
             <div class="lugar">
-                <p>${posicao+1}</p>
+                <p>${posicao + 1}º</p>
             </div>
-            <p>${item.concluida}</p>
             <p>${item.tarefa}</p>
-            <img src="./img/Sino.png" alt="alerta" class="invert-on-hover" onclick="concluirTarefa(${posicao})">
+            <img src="./img/checked.png" alt="alerta" class="imgAdmin" onclick="concluirItem(${posicao})">
+            <img src="./img/trash.png" alt="alerta" class="imgAdmin" onclick="deletarItem(${posicao})">
           </li>
 
-          `
-    })
-    
-    filaCompleta.innerHTML = novaLi
+      `
+  })
+
+  filaCompleta.innerHTML = novaLi
 }
 
-function concluirTarefa(posicao){
-    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
-    mostrarTarefas()
+function concluirItem(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+  mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1)
+  mostrarTarefas()
 }
 
 button.addEventListener('click', adicionarNovaTarefa)
-
+buttonAleatorio.addEventListener('click', adicionarAletorio)
